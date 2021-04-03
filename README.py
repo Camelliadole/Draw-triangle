2520de3a701b98d4fafa9e3a36efc4814439c288
Camellia Do Le <81404656+Camelliadole@users.noreply.github.com>
2021-04-03 18:35:05 +0700

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,17 @@
+# Draw-triangle
+n = 7
+j = (n+1)/2
+i = 1
+while i <= j:
+    if i == 1:
+        print ("*"*n)
+    else:
+        a = "*"*n
+        for m in range(0, int(i-1)):
+            a = a.replace(a[m], " ")
+            #break
+        for n in range(n-i+1, n):
+            a = a.replace(str(a[n]), " ")
+            #break
+        print (a)
+    i += 1
