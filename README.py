dc4311c4af33bf2d453d3ab2761bf88be5ab174b
Camellia Do Le <81404656+Camelliadole@users.noreply.github.com>
2021-04-05 19:12:10 +0700

    Update README.py

--- README.py
+++ README.py
@@ -1,18 +1,5 @@
 # Draw-triangle
-n = 7
-j = (n+1)/2
-i = 1
-while i <= j:
-    if i == 1:
-        print ("*"*n)
-    else:
-        a = "*"*n
-        for m in range(0, int(i-1)):
-            a = a.replace(a[m], " ")
-            #break
-        for n in range(n-i+1, n):
-            a = a.replace(str(a[n]), " ")
-            #break
-        print (a)
-    i += 1
- #this is my idea to solve this pro, but don't know why it not run correctly
+n = int(input("nhap so le nha ban iu: "))
+for i in range (1, int((n+1)/2+1)):
+    print(" "*(i-1) + "*"*(n-2*i+2) + " "*(i-1))
+
